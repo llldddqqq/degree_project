172566a5fd1d34e53b23849e9c04e3c18145e680
llldddqqq <ldq-990730@163.com>
2021-05-03 18:50:12 +0800

    Update Search.py

--- Group_14/Group 14/Search.py
+++ Group_14/Group 14/Search.py
@@ -9,11 +9,10 @@ def search(keystring):
     js = file.read()
     dicread = json.loads(js)
     file.close()
-    N = dicread['N']
+    N = 455391
     k = 1
     b = 0.75
-    avg = dicread['avg_doclen']
-    print(N,avg)
+    avg = 111.58179674170108
     stemmer = porter.PorterStemmer()
     stopwords = set()
     with open('stopwords.txt', 'r') as f:
@@ -32,9 +31,9 @@ def search(keystring):
     for term in keylist:
         dicij = {}
         i = 0
-        for id in dicread['house_index_info']:
-            if term in dicread['house_index_info'][id]:
-                fij = dicread['house_index_info'][id][term]
+        for id in dicread:
+            if term in dicread[id]:
+                fij = dicread[id][term]
                 dicij[id] = fij
                 i = i + 1
                 dic_ni[term] = i
@@ -44,12 +43,12 @@ def search(keystring):
                 dic_ni[term] = i
             dic_ij[term] = dicij
     bmij = {}
-    for docid in dicread['house_index_info']:
+    for docid in dicread:
         sim = 0
         for term in keylist:
             fij = dic_ij[term][docid]
             ni = dic_ni[term]
-            len = dicread['indoclen'][docid]
+            len = dicread[docid]['len']
             sim = sim + (fij * (1 + k) / (fij + k * (1 - b + ((b * len) / avg)))) * math.log(
                 ((N - ni + 0.5) / (ni + 0.5)), 2)
         bmij[docid] = sim
@@ -61,8 +60,8 @@ def search(keystring):
     return keylist
 
 
-#start_search = time.time()
-#search = search('dublin')
-#end_search = time.time()
-#print(search)
-#print('search time:', str(end_search - start_search))
+start_search = time.time()
+search = search('dublin')
+end_search = time.time()
+# print(search)
+print('search time:', str(end_search - start_search))
